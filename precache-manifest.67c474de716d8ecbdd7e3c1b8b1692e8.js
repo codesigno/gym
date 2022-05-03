a9dd9614e03bda0af9e59b75b7939533bf3ad8d4
Natalia <onataliadelina@gmail.com>
2022-05-03 09:58:46 +0200

    Updates

---
+++ precache-manifest.67c474de716d8ecbdd7e3c1b8b1692e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5fd0a43affa876e2374cd9e6f25e48",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ea6e42467145859292a",
+    "url": "./static/css/main.a4a56072.chunk.css"
+  },
+  {
+    "revision": "705c96f1bda06229169c",
+    "url": "./static/js/2.9d62c254.chunk.js"
+  },
+  {
+    "revision": "b0840287860008bab24c7966d74ee4ac",
+    "url": "./static/js/2.9d62c254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea6e42467145859292a",
+    "url": "./static/js/main.f47cd6e2.chunk.js"
+  },
+  {
+    "revision": "2bc20bb9523d1033a237",
+    "url": "./static/js/runtime-main.99ffbf97.js"
+  },
+  {
+    "revision": "4290131e01bd54247563afe47da086a2",
+    "url": "./static/media/men-figur.4290131e.png"
+  }
+]);
